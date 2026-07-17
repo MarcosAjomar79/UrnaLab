0efe5483306c83470a29196d1e166e78d6aac41a
MarcosAjomar79 <ajomarjunior2@gmail.com>
2026-07-17 13:39:12 -0300

    Cria e configura a tela de login.

--- src/UrnaLab.App/TelaLogin.Designer.cs
+++ src/UrnaLab.App/TelaLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UrnaLab.App;
 
-partial class Form1
+partial class TelaLogin
 {
     /// <summary>
     ///  Required designer variable.
@@ -28,10 +28,17 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
+        SuspendLayout();
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
-        Text = "Form1";
+        Name = "Form1";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "UrnaLab - Urna Eleitoral Grêmio";
+        ResumeLayout(false);
     }
 
     #endregion
--- src/UrnaLab.App/TelaLogin.cs
+++ src/UrnaLab.App/TelaLogin.cs
@@ -1,8 +1,8 @@
 namespace UrnaLab.App;
 
-public partial class Form1 : Form
+public partial class TelaLogin : Form
 {
-    public Form1()
+    public TelaLogin()
     {
         InitializeComponent();
     }
