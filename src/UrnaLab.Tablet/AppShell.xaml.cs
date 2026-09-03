eb8edb67985746639bfd4c476ec62c2997f0d415
MarcosAjomar79 <ajomarjunior2@gmail.com>
2026-09-02 23:58:35 -0300

    Cria projeto base UrnaLab Tablet em .NET MAUI

---
+++ src/UrnaLab.Tablet/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UrnaLab.Tablet
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/UrnaLab.Tablet/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace UrnaLab.Tablet
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
